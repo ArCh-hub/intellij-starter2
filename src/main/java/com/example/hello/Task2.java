6946121d40589c2ac37f22dacfa209caf4143579
ilove <ilovechashurin@gmail.com>
2021-04-11 21:37:44 +0300

    Just add line of code.

--- src/main/java/com/example/hello/Task2.java
+++ src/main/java/com/example/hello/Task2.java
@@ -3,5 +3,7 @@ package com.example.hello;
 public class Task2 {
     public static void main(String[] args) {
         System.out.println("Hello Intellij update");
+
+        System.out.println("New func");
     }
 }
