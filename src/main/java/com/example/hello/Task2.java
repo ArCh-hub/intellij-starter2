19827dc95419a85fa9a18985d395feeb15ed0e0c
ilove <ilovechashurin@gmail.com>
2021-04-11 23:00:39 +0300

    add line in diff branch

--- src/main/java/com/example/hello/Task2.java
+++ src/main/java/com/example/hello/Task2.java
@@ -3,5 +3,6 @@ package com.example.hello;
 public class Task2 {
     public static void main(String[] args) {
         System.out.println("Hello Intellij update");
+        System.out.println("another branch");
     }
 }
